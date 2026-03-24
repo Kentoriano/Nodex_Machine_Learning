10c400fd1d75e06133569d466f9738942d85901d
David <alasthor3245@gmail.com>
2026-03-23 22:21:30 -0500

    Add two new use cases and collaborator implementation

--- app.py
+++ app.py
@@ -20,5 +20,16 @@ def alien():
 def netflix():
     return render_template("netflix.html")
 
+@app.route("/fraud")
+def fraud():
+    return render_template("fraud.html")
+
+@app.route("/medical")
+def medical():
+    return render_template("medical.html")
+
+@app.route("/sales")
+def sales():
+    return render_template("sales.html")
 if __name__ == "__main__":
     app.run(debug=True)
